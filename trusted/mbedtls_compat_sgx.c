99d53978aeef6a00774d5b6fbb50bb83063c694b
Fábio Silva <fabio.fernando.osilva@gmail.com>
2019-05-02 16:06:44 -0300

    Improve rand() function
    - also rename glue -> mbedtls_compat_sgx

--- trusted/mbedtls_compat_sgx.c
+++ trusted/mbedtls_compat_sgx.c
@@ -15,18 +15,19 @@
  *
  * Authors:
  *     Fan Zhang <bl4ck5unxx@gmail.com>
+ *     Fábio Silva <fabio.fernando.osilva@gmail.com>
  *
  * This is a glue file calling ocalls.
  */
 
 #include <stdarg.h>
-#include <stdio.h>      /* vsnprintf */
-#include "glue.h"
+#include <stdio.h> /* vsnprintf */
+#include "mbedtls_compat_sgx.h"
 #include "sgx.h"
 #include "sgx_trts.h"
 
 // real ocall to be implemented in the Application
-extern int ocall_print_string(int* ret, char *str);
+extern int ocall_print_string(int *ret, char *str);
 int printf_sgx(const char *fmt, ...)
 {
     int ret;
@@ -41,31 +42,35 @@ int printf_sgx(const char *fmt, ...)
 }
 
 // ocall for entropy collection
-int mbedtls_hardware_poll(void *data, unsigned char *output, size_t len, size_t *olen )
+int mbedtls_hardware_poll(void *data, unsigned char *output, size_t len, size_t *olen)
 {
     (void)data;
     sgx_status_t st = sgx_read_rand(output, len);
-    if (st != SGX_SUCCESS) {
+    if (st != SGX_SUCCESS)
+    {
         printf_sgx("hardware_poll fails with %d\n", st);
         *olen = -1;
         return -1;
     }
-    else {
+    else
+    {
         *olen = len;
         return 0;
     }
 }
 
 // ocall for hardware RNG
-int mbedtls_sgx_drbg_random( void *p_rng, unsigned char *output, size_t out_len )
+int mbedtls_sgx_drbg_random(void *p_rng, unsigned char *output, size_t out_len)
 {
-    if (!output) {
+    if (!output)
+    {
         printf_sgx("mbedtls_sgx_drbg receives NULL");
         return -1;
     }
     (void)p_rng;
     sgx_status_t st = sgx_read_rand(output, out_len);
-    if (st != SGX_SUCCESS) {
+    if (st != SGX_SUCCESS)
+    {
         printf_sgx("mbedtls_sgx_drbg fails with %d\n", st);
         return -1;
     }
@@ -78,7 +83,11 @@ int rand()
 {
     int random_val = 0;
 
-    sgx_read_rand((unsigned char *) &random_val, sizeof(random_val));
-
+    sgx_status_t ret = sgx_read_rand((unsigned char *)&random_val, sizeof(random_val));
+    if (SGX_SUCCESS != ret)
+    {
+        printf_sgx("rand fails with %d\n", SGX_SUCCESS);
+        return -1;
+    }
     return random_val;
 }
